f955adf8accd7122495573d7d65e9eb5057c5683
ieh <hodellurt@gmail.com>
2021-03-03 00:32:24 +0900

    consistency

--- src/deduction.lean
+++ src/deduction.lean
@@ -1,5 +1,4 @@
 import syntax
-import semantics
 
 def subst (p : ℕ) (φ : formula) : formula → formula
 | (q : ℕ)  := if p = q then φ else q
@@ -20,54 +19,4 @@ notation `𝐊` := prv
 infix ` ⊢ₖ `:55 := prv
 
 def consistent (Γ : set formula) : Prop := ⊥ₘ ∉ 𝐊 Γ
-
-open semantics
-
-universe variable u
-
-variable {W : Type u}
-variable F : kripke_frame W
-variables p q : ℕ
-variables φ ψ χ : formula
-#check distribution
-#check satisfy F
-
-theorem us_semantics : F ⊧ φ → F ⊧ (subst p ψ φ) :=
-begin
-  intros h V w,
-  let V' : ℕ → set W := fun e u, [F, V, u] ⊧ subst p ψ e,
-  have i : ∀ v, ([F, V', v] ⊧ φ) ↔ ([F, V, v] ⊧ subst p ψ φ),
-  { clear h,
-    induction φ,
-    { exact λ v, ⟨λ h₀, h₀, λ h₀, h₀⟩ },
-    { exact λ v, ⟨λ h₀, h₀, λ h₀, h₀⟩ },
-    { exact λ v, ⟨λ h₀ s, (φ_ih_ᾰ_1 v).1 (h₀ ((φ_ih_ᾰ v).2 s)), λ h₀ s, (φ_ih_ᾰ_1 v).2 (h₀ ((φ_ih_ᾰ v).1 s))⟩ },
-    { exact λ v, ⟨λ h u rvu, (φ_ih u).1 (h u rvu), λ h u rvu, (φ_ih u).2 (h u rvu)⟩ } }, 
-  have l : [F, V', w] ⊧ φ := h V' w,
-  exact (i w).1 l
-end
-
-theorem mp_semantics : F ⊧ φ →ₘ ψ → F ⊧ φ → F ⊧ ψ :=
-λ (h : F ⊧ φ →ₘ ψ) (h₀ : F ⊧ φ) (V : ℕ → set W) (w : W), (h V w) (h₀ V w) 
-
-theorem ak_semantics : F ⊧ φ →ₘ ψ →ₘ φ := λ V w h h₀, h
-
-theorem as_semantics : F ⊧ (φ →ₘ ψ →ₘ χ) →ₘ (φ →ₘ ψ) →ₘ φ →ₘ χ := λ V w h h₀ h₁, h h₁ (h₀ h₁)
-
-theorem an_semantics : F ⊧ (¬ₘφ →ₘ ¬ₘψ) →ₘ ψ →ₘ φ :=
-by { intros V w h, simp [satisfy], contrapose, exact h }
-
-theorem soundness : ∅ ⊢ₖ φ → F ⊧ φ :=
-begin
-  assume h,
-  induction h,
-  { exact us_semantics _ h_p _ _ h_ih },
-  { exfalso, exact h_ᾰ },
-  { exact mp_semantics _ _ _ h_ih_ᾰ h_ih_ᾰ_1 },
-  { exact necessitation _ _ h_ih },
-  { exact ak_semantics _ _ _ },
-  { exact as_semantics _ _ _ _ },
-  { exact an_semantics _ _ _ },
-  { exact distribution _ _ _ }
-end
 
--- src/semantics.lean
+++ src/semantics.lean
@@ -1,11 +1,17 @@
 import data.int.parity
 import syntax
+import deduction
 open classical
 
 universe variables u 
 
 structure kripke_frame (W : Type u) :=
 (relation : W → W → Prop)
+
+inductive singleton : Type u 
+| s : singleton
+
+def trivial_frame : kripke_frame singleton := ⟨λ x y, true⟩
 
 namespace rel
 
@@ -29,12 +35,12 @@ def frames {W : Type u} (F : kripke_frame W) (p : formula) := ∀ V w, satisfy F
 notation `[`F`, `V`, `w`]` ` ⊧ `p := satisfy F V w p
 infix ` ⊧ `:50 := frames
 
-
 namespace semantics
 
 variable {W : Type u}
 variable F : kripke_frame W
-variables φ ψ : formula
+variables p q : ℕ
+variables φ ψ χ : formula
 local infix ` ≺ `:50 := F.relation
 
 theorem distribution : F ⊧ □(φ →ₘ ψ) →ₘ □φ →ₘ □ψ :=
@@ -54,7 +60,7 @@ theorem necessitation : F ⊧ φ → F ⊧ □φ :=
   (h₁ : w ≺ v),
 h₀ V v
 
-variable p : ℕ
+/--
 theorem trans_iff_4
   : rel.transitive F ↔ F ⊧ □p →ₘ □□p :=
 begin
@@ -73,5 +79,45 @@ begin
       sorry },
     sorry }
 end
+-/
+
+lemma us_soundness : F ⊧ φ → F ⊧ (subst p ψ φ) :=
+begin
+  intros h V w,
+  let V' : ℕ → set W := fun e u, [F, V, u] ⊧ subst p ψ e,
+  have i : ∀ v, ([F, V', v] ⊧ φ) ↔ ([F, V, v] ⊧ subst p ψ φ),
+  { clear h,
+    induction φ,
+    { exact λ v, ⟨λ h₀, h₀, λ h₀, h₀⟩ },
+    { exact λ v, ⟨λ h₀, h₀, λ h₀, h₀⟩ },
+    { exact λ v, ⟨λ h₀ s, (φ_ih_ᾰ_1 v).1 (h₀ ((φ_ih_ᾰ v).2 s)), λ h₀ s, (φ_ih_ᾰ_1 v).2 (h₀ ((φ_ih_ᾰ v).1 s))⟩ },
+    { exact λ v, ⟨λ h u rvu, (φ_ih u).1 (h u rvu), λ h u rvu, (φ_ih u).2 (h u rvu)⟩ } }, 
+  have l : [F, V', w] ⊧ φ := h V' w,
+  exact (i w).1 l
+end
+
+lemma mp_soundness : F ⊧ φ →ₘ ψ → F ⊧ φ → F ⊧ ψ := λ h h₀ V w, (h V w) (h₀ V w) 
+
+lemma ak_soundness : F ⊧ φ →ₘ ψ →ₘ φ := λ V w h h₀, h
+
+lemma as_soundness : F ⊧ (φ →ₘ ψ →ₘ χ) →ₘ (φ →ₘ ψ) →ₘ φ →ₘ χ := λ V w h h₀ h₁, h h₁ (h₀ h₁)
+
+lemma an_soundness : F ⊧ (¬ₘφ →ₘ ¬ₘψ) →ₘ ψ →ₘ φ := by { intros V w h, simp [satisfy], contrapose, exact h }
+
+theorem soundness : ∅ ⊢ₖ φ → F ⊧ φ :=
+begin
+  assume h,
+  induction h,
+  { exact us_soundness _ h_p _ _ h_ih },
+  { exfalso, exact h_ᾰ },
+  { exact mp_soundness _ _ _ h_ih_ᾰ h_ih_ᾰ_1 },
+  { exact necessitation _ _ h_ih },
+  { exact ak_soundness _ _ _ },
+  { exact as_soundness _ _ _ _ },
+  { exact an_soundness _ _ _ },
+  { exact distribution _ _ _ }
+end
+
+theorem consis : consistent ∅ := λ h, soundness trivial_frame ⊥ₘ h (λ n, ∅) singleton.s
 
 end semantics
